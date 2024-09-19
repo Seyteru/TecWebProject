dba5bc53abff6d92141fccbfd68fc6ce42457789
Seyteru <s.cioffi2001@gmail.com>
2024-09-19 02:58:40 +0200

    Add .gitignore with node_modules

--- frontend/src/app/components/article-creation/article-creation.component.ts
+++ frontend/src/app/components/article-creation/article-creation.component.ts
@@ -3,11 +3,13 @@ import { ArticleService } from '../../services/article.service';
 import { Router } from '@angular/router';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { MatIcon } from '@angular/material/icon';
+import { LMarkdownEditorModule } from 'ngx-markdown-editor'
+import { MarkdownModule } from 'ngx-markdown'
 
 @Component({
   selector: 'app-article-creation',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, MatIcon],
+  imports: [ReactiveFormsModule, FormsModule, MatIcon, LMarkdownEditorModule, MarkdownModule],
   templateUrl: './article-creation.component.html',
   styleUrl: './article-creation.component.scss'
 })
@@ -18,6 +20,11 @@ export class ArticleCreationComponent {
   private router = inject(Router);
   createArticleForm: FormGroup;
   errorMsg: string = '';
+  articleContent: string = '';
+
+  onContentChange(content: string){
+    this.articleContent = content;
+  }
 
   constructor(){
     this.createArticleForm = this.formBuilder.group({
